9c5138a87b87aaa1247a962e62dc1c984ad5822a
@BenjaminWegener <wegeneredv@gmail.com>
2022-08-24 16:23:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -464,7 +464,7 @@ function loop(time) {
 
 function accessWebcam(video) {
   return new Promise((resolve, reject) => {
-    const mediaConstraints = { audio: false, video: { width: {ideal: WIDTH}, height: {ideal: HEIGHT}, brightness: {ideal: 2} } };
+    const mediaConstraints = { audio: false, video: { brightness: {ideal: 2} } };
     navigator.mediaDevices.getUserMedia(mediaConstraints).then(mediaStream => {
       video.srcObject = mediaStream;
       video.setAttribute('playsinline', true);
